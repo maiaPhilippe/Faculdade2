9c2ea9eda8a85e1b1946a4d1425dfd79cf1c51c6
maiaPhilippe <phil.maia@gmail.com>
2016-05-23 11:04:58 -0300

    Update

--- prog1_av2_1.c
+++ prog1_av2_1.c
@@ -5,23 +5,23 @@ int main(void) {
         char nome[30];
         float nota;
     };
-    struct Alunos alunos[20];
-    struct Alunos aux[20];
+    struct Alunos alunos[30];
+    struct Alunos aux[30];
     int count,change,i,final;
     final = 1;
     count = 0;
     i = 0;
     change = 0;
     printf("\nOla meu nome e Philippe Maia e este programa pode ser visto no seguinte endereço do github:\n https://github.com/maiaPhilippe/trabalhoAv2/blob/master/prog1_av2_1.c\n");
-    for(count = 0 ; count < 20 ; count++) {
+    for(count = 0 ; count < 30 ; count++) {
         printf("\nNome do Aluno:\n");
         scanf("%s", alunos[count].nome) ; 
         printf("Entre com a nota de %s\n", alunos[count].nome);
         scanf("%f", &alunos[count].nota);
     }
     while (final > 0) {
-           for (count = 0; count < 20;count++) {
-                for (i = 0; i < 20 ;i++) {
+           for (count = 0; count < 30;count++) {
+                for (i = 0; i < 30 ;i++) {
                     if (alunos[i].nota < alunos[i+1].nota) {
                         aux[i] = alunos[i];
                         alunos[i] = alunos[i+1];
@@ -41,7 +41,7 @@ int main(void) {
     printf("\n    Listagem da AV1\n\n");
     printf("Nome\t\t\tNota\n");
     printf("---------------------------------------");
-    for(count = 0 ; count < 20 ; count++) {
+    for(count = 0 ; count < 30 ; count++) {
          printf("\n%s\t\t\t%.2f\n", alunos[count].nome,alunos[count].nota);
     }
 
